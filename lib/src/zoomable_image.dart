363aabd67b980ad108b894a88e2077e74d05aade
perlatus <perlatus@git.email.vczf.io>
2017-07-09 19:38:44 -0400

    Make offset from actual top left corner of image

--- lib/src/zoomable_image.dart
+++ lib/src/zoomable_image.dart
@@ -31,22 +31,35 @@ class _ZoomableImageState extends State<ZoomableImage> {
   Offset _startingFocalPoint;
 
   Offset _previousOffset;
-  Offset _offset = Offset.zero;
+  Offset _offset;
 
   double _previousZoom;
   double _zoom = 1.0;
 
   @override
-  Widget build(BuildContext ctx) {
-    if (_image == null) {
-      return new Container();
+  Widget build(BuildContext ctx) => _image == null
+      ? new Container()
+      : new LayoutBuilder(builder: _buildLayout);
+
+  Widget _buildLayout(BuildContext ctx, BoxConstraints constraints) {
+    if (_offset == null) {
+      Size imageSize = new Size(
+        _image.width.toDouble(),
+        _image.height.toDouble(),
+      );
+
+      Size canvas = constraints.biggest;
+      Size fitted = _containmentSize(canvas, imageSize);
+
+      Offset delta = canvas - fitted;
+      _offset = delta / 2.0; // Centers the image
     }
 
     return new GestureDetector(
       child: _child(),
       onTap: widget.onTap,
       onScaleStart: _handleScaleStart,
-      onScaleUpdate: (d) => _handleScaleUpdate(ctx.size, d),
+      onScaleUpdate: _handleScaleUpdate,
     );
   }
 
@@ -61,12 +74,13 @@ class _ZoomableImageState extends State<ZoomableImage> {
   }
 
   void _handleScaleStart(ScaleStartDetails d) {
+    print("starting scale at ${d.focalPoint} from $_offset $_zoom");
     _startingFocalPoint = d.focalPoint;
     _previousOffset = _offset;
     _previousZoom = _zoom;
   }
 
-  void _handleScaleUpdate(Size size, ScaleUpdateDetails d) {
+  void _handleScaleUpdate(ScaleUpdateDetails d) {
     double newZoom = _previousZoom * d.scale;
     if (newZoom >= _scale) {
       return;
@@ -114,6 +128,23 @@ class _ZoomableImageState extends State<ZoomableImage> {
   }
 }
 
+// Given a canvas and an image, determine what size the image should be to be contained in but not
+// exceed the canvas while preserving its aspect ratio.
+Size _containmentSize(Size canvas, Size image) {
+  double canvasRatio = canvas.width / canvas.height;
+  double imageRatio = image.width / image.height;
+
+  if (canvasRatio < imageRatio) {
+    // fat
+    return new Size(canvas.width, canvas.width / imageRatio);
+  } else if (canvasRatio > imageRatio) {
+    // skinny
+    return new Size(canvas.height * imageRatio, canvas.height);
+  } else {
+    return canvas;
+  }
+}
+
 class _ZoomableImagePainter extends CustomPainter {
   const _ZoomableImagePainter({this.image, this.offset, this.zoom});
 
@@ -122,12 +153,15 @@ class _ZoomableImagePainter extends CustomPainter {
   final double zoom;
 
   @override
-  void paint(Canvas canvas, Size size) {
+  void paint(Canvas canvas, Size canvasSize) {
+    Size imageSize = new Size(image.width.toDouble(), image.height.toDouble());
+    Size targetSize = _containmentSize(canvasSize, imageSize) * zoom;
+
     paintImage(
-        canvas: canvas,
-        rect: offset & (size * zoom),
-        image: image,
-        fit: BoxFit.contain);
+      canvas: canvas,
+      rect: offset & targetSize,
+      image: image,
+    );
   }
 
   @override
